7e10c31228ec47f8bf3628d9c14fba8058847348
Olivia Dunbar <odunbar3@gmail.com>
2020-04-27 00:22:23 -0400

    Hopeful heroku fix

--- server/routes/TutorsRouter.js
+++ server/routes/TutorsRouter.js
@@ -1,5 +1,5 @@
 const tutors = require('../controllers/TutorController');
-const config = require('../config/config.js')
+// const config = require('../config/config.js')
 const express = require('express')
 const mongoose = require('mongoose')
 const tutorsRouter = express.Router();
