ceccbcb17d6fcc19448e76e260a7e70280e049b0
Olivia Dunbar <odunbar3@gmail.com>
2020-04-27 00:15:20 -0400

    Changes that weren't updated the first time

---
+++ client/src/components/UpdateActivation/UpdateActivation.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import "./UpdateActivation.css";
+import HomeButton from "../HomeButton/HomeButton"
+import userData from "../../userData"
+
+export default class UpdateActivation extends Component {
+    componentDidMount(){
+        console.log(this.props.location.pathname);
+        userData.activateUpdates(this.props.location.pathname);
+
+    }
+    render() {
+        return (
+            <div className = "App-header">
+                <h1 className = "activation">Thank you for updating your post!</h1>
+                <h1>Your updates will now show up in the tutor list</h1>
+                <HomeButton/>
+            </div>
+        )
+    }
+}
